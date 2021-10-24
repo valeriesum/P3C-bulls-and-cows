4d642d5c82e0314296d18a67d4a244b5ff3d661f
davvd123 <335671335@gapps.yrdsb.ca>
2021-10-24 13:37:37 -0400

    Changed World Size
    changed size from 600,400 to 700,500

--- Bulls-and-Cows/MyWorld.java
+++ Bulls-and-Cows/MyWorld.java
@@ -16,7 +16,7 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(700, 500, 1);
         Stack<String> wordStack = new Stack<String>();
         ArrayList<String> allWord = new ArrayList<String>();
     }
