7cf72cb08574176a6cbc6c8e6773a2a8d6bb941b
davvd123 <335671335@gapps.yrdsb.ca>
2021-10-24 13:43:01 -0400

    changed string stack to character stack

--- Bulls-and-Cows/MyWorld.java
+++ Bulls-and-Cows/MyWorld.java
@@ -17,7 +17,7 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(700, 500, 1);
-        Stack<String> wordStack = new Stack<String>();
+        Stack<Character> wordStack = new Stack<Character>();
         ArrayList<String> allWord = new ArrayList<String>();
     }
 }
