a244f7c756ca46c7fce8b5687ee0648d30a6d2cb
Valerie Sum <341116028@gapps.yrdsb.ca>
2021-10-26 09:02:56 -0400

    start page code

---
+++ Bulls-and-Cows/InstructionsAndDifficulty.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The instructions page and available difficulty levels for the game.
+ * 
+ * @author Valerie Sum 
+ * @version Oct. 26/21
+ */
+public class InstructionsAndDifficulty extends World
+{
+
+    /**
+     * Constructor for objects of class InstructionsAndDifficulty.
+     * 
+     */
+    public InstructionsAndDifficulty()
+    {    
+        // Create a new world with 800x600 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1); 
+    }
+}
