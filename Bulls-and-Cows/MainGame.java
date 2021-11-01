8f7b653b675c017dd3c9b21746a853e0414e2ea5
Catherine Wang <92929459+why-wontitwork@users.noreply.github.com>
2021-11-01 09:15:47 -0400

    a very very simple counter

--- Bulls-and-Cows/MainGame.java
+++ Bulls-and-Cows/MainGame.java
@@ -21,5 +21,6 @@ public class MainGame extends World
         
         backgroundMusic.setVolume(30);
         backgroundMusic.play(); // Playing background music.
+        
     }
 }
---
+++ Bulls-and-Cows/counter.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class counter here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class counter extends World
+{
+
+    /**
+     * Constructor for objects of class counter.
+     * 
+     */
+    public counter()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        int counter = 5;
+        counter--;
+    }
+}
