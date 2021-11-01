b0c090888db574172ed4a18e06257ad09713717d
Valerie Sum <341116028@gapps.yrdsb.ca>
2021-11-01 10:37:38 -0400

    alphabet + compare

--- Bulls-and-Cows/MainGame.java
+++ Bulls-and-Cows/MainGame.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.Set.*;
 /**
  * Primary game portion of the program.
  * 
@@ -11,9 +11,13 @@ public class MainGame extends World
     GreenfootSound backgroundMusic = new GreenfootSound("Frog in the Well.mp3");
     //Counter for number of tries 
     private int counter = 5;
+    //User guess
     Stack<String> guess = new Stack<String>();
-    
+    //Number of letters in word
     private static int numLetters = 4;
+    //Array of the alphabet
+    char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    
     /**
      * Constructor for objects of class MainGame.
      * 
@@ -27,13 +31,30 @@ public class MainGame extends World
         backgroundMusic.play(); // Playing background music.
     }
     
+    /**
+     * This method 
+     */
+    public boolean check(char letter)
+    {
+        boolean check = false;
+        for(char c : alphabet)
+        {
+            if(letter == c)
+            {
+                check = true;
+            }
+        }
+        return check;
+    }
+        
     public void act()
     {
-        while(!guessCorrect)
+        while(counter != 0)
         {
             while(guess.size() < 4)
             {
-                
+                check(Greenfoot.getKey().charAt(0));
             }    
         }
+    }
 }
