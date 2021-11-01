1976e29a852baf374802e7dbcc70a62e0638f279
Valerie Sum <341116028@gapps.yrdsb.ca>
2021-11-01 09:29:42 -0400

    took away difficulty levels

--- Bulls-and-Cows/InstructionsAndDifficulty.java
+++ Bulls-and-Cows/InstructionsAndDifficulty.java
@@ -17,21 +17,14 @@ public class InstructionsAndDifficulty extends World
     {    
         // Create a new world with 800x600 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
-        
-        //Creating buttons for difficulty levels
-        Button beginner = new Button();
-        addObject(beginner, 150, 500);
-        String begLabel = "Beginner\n3 Letters";
-        addObject(new Text(begLabel), 150, 500);
-        
-        Button regular = new Button();
-        addObject(regular, 400, 500);
-        String regLabel = "Regular\n4 Letters";
-        addObject(new Text(regLabel), 400, 500);
-        
-        Button intermediate = new Button();
-        addObject(intermediate, 650, 500);
-        String interLabel = "Intermediate\n5 Letters";
-        addObject(new Text(interLabel), 650, 500);
+    }
+    
+    public void act()
+    {
+        // If player presses "E", the main game begins.
+        if(Greenfoot.isKeyDown("e"))
+        {
+            Greenfoot.setWorld(new MainGame());
+        }
     }
 }
