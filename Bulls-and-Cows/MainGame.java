6d1d870530dcf14059cabc4bbac4f4729cd0ae6a
davvd123 <335671335@gapps.yrdsb.ca>
2021-11-01 09:56:52 -0400

    Reader and Word
    Implemented Reader and Word generator

--- Bulls-and-Cows/MainGame.java
+++ Bulls-and-Cows/MainGame.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.ArrayList;
 /**
  * Primary game portion of the program.
  * 
@@ -9,7 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class MainGame extends World
 {
     GreenfootSound backgroundMusic = new GreenfootSound("Frog in the Well.mp3");
-    
+    private String theWord;
     /**
      * Constructor for objects of class MainGame.
      * 
@@ -17,11 +17,20 @@ public class MainGame extends World
     public MainGame()
     {    
         // Create a new world with 800x600 cells with a cell size of 1x1 pixels.
-        super(800, 600, 1); 
-        
+        super(800, 600, 1);
+    
         backgroundMusic.setVolume(30);
         backgroundMusic.play(); // Playing background music.
         
-        
+        ArrayList<String> myList = new ArrayList<String>();
+        try{
+            Reader.readInto(myList);
+        } catch(Exception e) {
+        }
+        theWord = myList.get((int)Math.floor(Math.random()*myList.size()));
+    }
+    
+    public String returnGuess(){
+        return theWord;
     }
 }
