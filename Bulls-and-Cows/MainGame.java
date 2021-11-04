b8d8016416e83cb72390646a3e101146e94d7156
davvd123 <335671335@gapps.yrdsb.ca>
2021-11-04 08:52:03 -0400

    ok

--- Bulls-and-Cows/MainGame.java
+++ Bulls-and-Cows/MainGame.java
@@ -14,6 +14,7 @@ public class MainGame extends World
     private static final int COUNTER = 10;
     //User guess
     Stack<Character> guess = new Stack<Character>();
+    Stack<Character> displayGuess = new Stack<Character>();
     //Number of letters in word
     private static final int numLetters = 4;
     //Array of the alphabet
@@ -57,7 +58,10 @@ public class MainGame extends World
         counter = COUNTER;
 
         counter = Math.max(0,counter);
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 0ab3d37 (Revert "Fixed issues")
     }
 
     public String returnGuess(){
@@ -97,13 +101,14 @@ public class MainGame extends World
         Integer theCounter = new Integer(counter);
         if(counter >0)
         {
-            if(guess.size() <= 4)
+            if(guess.size() < 4)
             {
                 if (userInput != null){
 
                     if(check(userInput)){
                         char programInput = userInput.charAt(0);
                         guess.push(programInput);
+                        displayGuess.push(programInput);
                         if (guess.size()==1){
                             addObject(new Text(programInput, 85, 0, 0, 0, 255, 216, 137), 330, 480);
                         } else if (guess.size()==2){
@@ -121,32 +126,51 @@ public class MainGame extends World
             }
 
         }
-        if (Greenfoot.isKeyDown("backspace")&&!guess.isEmpty()){
+        if (Greenfoot.isKeyDown("backspace")){
 
             if(guess.size()==1){
                 removeObjects(getObjectsAt(330, 470,null));
                 guess.pop();
+                displayGuess.pop();
             } else if(guess.size()==2){
                 removeObjects(getObjectsAt(430, 470,null));
                 guess.pop();
+                displayGuess.pop();
             } else if(guess.size()==3){
                 removeObjects(getObjectsAt(530, 470,null));
                 guess.pop();
+                displayGuess.pop();
             } else if(guess.size()==4){
                 removeObjects(getObjectsAt(630, 470,null));
                 guess.pop();
+                displayGuess.pop();
             }
-
+            
         }
         if (enterDown != Greenfoot.isKeyDown("enter")){
             enterDown = !enterDown;
             if (enterDown){
+<<<<<<< HEAD
                 displayPreviousBullsAndCows();
+=======
+>>>>>>> parent of 0ab3d37 (Revert "Fixed issues")
                 checkBullsAndCows();
                 displayCurrentBullsAndCows();
+<<<<<<< HEAD
                 clearScreen();
                 counter--;
                 
+=======
+                displayPreviousBullsAndCows();
+                bulls = 0;
+                cows = 0;
+                while(!displayGuess.isEmpty()){
+                    displayGuess.pop();
+                }
+                
+                clearScreen();
+                counter--;
+>>>>>>> parent of 0ab3d37 (Revert "Fixed issues")
             }
         }// record change
         if(counter == 0) //Player loses
@@ -218,9 +242,9 @@ public class MainGame extends World
         Integer theCows = new Integer(cows);
 
         String text = "";
-        for(Character c: guess)
+        for(Character c: displayGuess)
         {
-            text = Character.toString(c) + text;
+            text = Character.toString(c) + text ;
         }
         text = text + "\n";
 
